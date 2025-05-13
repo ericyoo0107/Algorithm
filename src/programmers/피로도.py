a83c290b8dbd77a06d4c9dc6b23579c2f1c9bb62
JunHyuk Yu <ericyoo0107@gmail.com>
2025-05-13 11:50:56 +0900

    [문제풀이] : 프로그래머스 피로도(라이브러리X) 풀이

--- src/programmers/피로도.py
+++ src/programmers/피로도.py
@@ -1,3 +1,32 @@
+def comp(k, dungeons):
+    result = []
+    if k == 1:
+        return [[i] for i in dungeons]
+
+    for i in range(len(dungeons)):
+        elem = dungeons[i]
+        for j in comp(k - 1, dungeons[:i] + dungeons[i + 1:]):
+            result.append([elem] + j)
+    return result
+
+
+def solution(k, dungeons):
+    cases = comp(len(dungeons), dungeons)
+    answer = 0
+    for case in cases:
+        tmpK = k
+        tmpAns = 0
+        for dungeon in case:
+            if dungeon[0] <= tmpK:
+                tmpAns += 1
+                tmpK -= dungeon[1]
+            else:
+                break
+        if answer < tmpAns:  answer = tmpAns
+    return answer
+
+
+'''
 import itertools
 
 def solution(k, dungeons):
@@ -13,6 +42,7 @@ def solution(k, dungeons):
             else: break
         if answer < tmpAns:  answer = tmpAns
     return answer
+'''
 
 k = 80
 dungeons = [[80,20],[50,40],[30,10]]
